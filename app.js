b7f99f2852f75d18c93231b74e3c913c5fee59d8
jon-cunicella <joncunicella@gmail.com>
2019-07-09 13:08:39 -0400

    Change folde rstructure

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./src/server/routes/index');
+var usersRouter = require('./src/server/routes/users');
 
 var app = express();
 
