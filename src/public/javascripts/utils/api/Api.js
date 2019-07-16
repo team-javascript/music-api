376d4e00af233fb5d2d34eb2a8766f7d0527af08
jon-cunicella <joncunicella@gmail.com>
2019-07-16 14:51:45 -0400

    Can fetch data from star wars api

--- src/public/javascripts/utils/api/Api.js
+++ src/public/javascripts/utils/api/Api.js
@@ -1,5 +1,12 @@
-export default function (){}
-
-class Api{
-    
-}
+export default function() {
+    return new Api();
+  }
+  
+  class Api {
+    getRequest(location, callback) {
+      fetch(location)
+        .then(response => response.json())
+        .then(callback)
+        .catch(err => console.log(err));
+    }
+  }
