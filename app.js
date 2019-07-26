1fdb8b901cb20fd0d166b6ce195ce5e5dcdd96b3
clayton-scherer <cscherer93@aol.com>
2019-07-25 20:59:43 -0400

    fixed

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-require('./src/server/models/db');
+require("./src/server/models/db");
 
-const artistRouter = require('./src/server/routes/artist-router');
-const albumRouter = require('./src/server/routes/album-router');
-const songRouter = require('./src/server/routes/song-router');
+const artistRouter = require("./src/server/routes/artist-router");
+const albumRouter = require("./src/server/routes/album-router");
+const songRouter = require("./src/server/routes/song-router");
 
 const app = express();
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use('/', indexRouter);
-app.use('/artists', artistRouter);
-app.use('/albums', albumRouter);
-app.use('/songs', songRouter);
+app.use("/artists", artistRouter);
+app.use("/albums", albumRouter);
+app.use("/songs", songRouter);
 
 module.exports = app;
